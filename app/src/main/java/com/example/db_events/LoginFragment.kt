9bb41d02b9520b1ff60c9a0d5aab417998f0c782
Petru Huja <useruserph@gmail.com>
2022-06-14 00:41:17 +0300

    cant get binding

--- app/src/main/java/com/example/db_events/LoginFragment.kt
+++ app/src/main/java/com/example/db_events/LoginFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 
 class LoginFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false)
+        binding.tryAgainButton.setOnClickListener{ view: View ->
+//            view.findNavController().navigate(R.id.action_gameOverFragment2_to_gameFragment)
+            view.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragment2ToGameFragment())
+        }
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/db_events/MainActivity.kt
+++ app/src/main/java/com/example/db_events/MainActivity.kt
@@ -4,19 +4,52 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
+//val register_button = findViewById<Button>(R.id.register_button)
+//register_button.setOnClickListener{v ->
+////            v.findNavController().navigate()
+//}
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val register_button = findViewById<Button>(R.id.register_button)
-        register_button.setOnClickListener{v ->
-//            v.findNavController().navigate()
-        }
+//        @Suppress("UNUSED_VARIABLE")
+//        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+//
+//        drawerLayout = binding.drawerLayout
+//
+//        val navController = this.findNavController(R.id.myNavHostFragment)
+//        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+//
+////        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+//
+//        navController.addOnDestinationChangedListener{ nc: NavController, nd: NavDestination, args: Bundle? ->
+//            if (nd.id == nc.graph.startDestination) {
+//                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+//            } else {
+//                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+//            }
+//        }
+//
+//        NavigationUI.setupWithNavController(binding.navView, navController)
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+//        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
